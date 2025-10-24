2869d940134ed8eb89064b966a34d18861f602d4
PABG1990 <pedro19908454@gmail.com>
2025-10-24 18:50:32 -0500

    Act_CFA

--- src/config/global.js
+++ src/config/global.js
@@ -152,7 +152,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/CF2_62650004_DU.zip',
       },
       {
         icono: 'fas fa-download',
